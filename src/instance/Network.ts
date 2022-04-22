9da81e4982670053dd79a22dcb8ed0774c84f5bf
quiquelhappy <1636461@uab.cat>
2022-04-22 18:21:17 +0200

    instance creation, cname and name modifiers

--- src/instance/Network.ts
+++ src/instance/Network.ts
@@ -1,27 +1,52 @@
 import { call } from "../http/Call";
+import Instance from "./Instance";
 
 export default class Network {
-    public readonly id: string;
-    public readonly name: string;
 
-    constructor(id: string, name: string) {
+    public readonly id: string;
+    private _name: string;
+    private _cname: string;
+
+    constructor(id: string, name: string, cname: string) {
         this.id = id;
-        this.name = name;
+        this._name = name;
+        this._cname = cname;
     }
 
+    public get name(): string { return this._name }
+    public get cname(): string { return this._cname }
+
     public static fromObject(object: any): Network {
-        return new Network(object.id, object.name)
+        return new Network(object.id, object.name, object.cname)
     }
 
     public async update(name?: string, cname?: string): Promise<Network> {
         const obj: any = {}
         if (name) obj.name = name
         if (cname) obj.cname = cname
-        return await call(`network/${this.id}`, obj, 'PATCH') as Network
+        const result = await call(`network/${this.id}`, obj, 'PATCH') as Network
+        this._cname = result.cname;
+        this._name = result.name;
+        return this
     }
 
     public async delete(): Promise<void> {
         return await call(`network/${this.id}`, undefined, 'DELETE')
+    }
+
+    public async createInstance(path: string): Promise<Instance> {
+        return Instance.fromObject(this, await call(`network/${this.id}/instance`, {
+            path: path
+        }))
+    }
+
+    public async getInstances(): Promise<Instance[]> {
+        const instances: any[] = await call(`network/${this.id}/instance`)
+        return instances.map(o => Instance.fromObject(this, o))
+    }
+
+    public async getInstance(id: string): Promise<Instance> {
+        return Instance.fromObject(this, await call(`network/${this.id}/instance/${id}`))
     }
 
 }
