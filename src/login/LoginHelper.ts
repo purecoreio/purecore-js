f0bb5b4cf159da1280e1378b761ee33efdfabf91
quiquelhappy <quiquelhappy@gmail.com>
2022-07-19 09:35:55 +0200

    use standard path for oauth authorize

--- src/login/LoginHelper.ts
+++ src/login/LoginHelper.ts
@@ -6,7 +6,7 @@ export default class LoginHelper {
     private static activeWindow: Window;
 
     public static async login(method: Method, scope: scope[], responseType: oAuthResponseType, clientId?: string, redirectURI?: string, state?: string, accessToken?: string): Promise<Token> {
-        return Token.fromObject(await Popup.openPopup(`/oauth/authorize/${method}/?scope=${scope.join(" ")}&response_type=${responseType}${clientId != null ? `&client_id=${clientId}` : ""}${redirectURI != null ? `&redirect_uri=${redirectURI}` : ""}${state != null ? `&state=${state}` : ""}${accessToken != null ? `&access_token=${accessToken}` : ""}`, "login"))
+        return Token.fromObject(await Popup.openPopup(`/oauth/authorize/?service=${method}&scope=${scope.join(" ")}&response_type=${responseType}${clientId != null ? `&client_id=${clientId}` : ""}${redirectURI != null ? `&redirect_uri=${redirectURI}` : ""}${state != null ? `&state=${state}` : ""}${accessToken != null ? `&access_token=${accessToken}` : ""}`, "login"))
     }
 
 }
