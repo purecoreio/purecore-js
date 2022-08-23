3e4223e8f3719e9309707c7ce4422a795770d95f
quiquelhappy <quiquelhappy@gmail.com>
2022-08-23 17:35:21 +0200

    profile pfp

--- src/user/Profile.ts
+++ src/user/Profile.ts
@@ -5,20 +5,21 @@ export default class Profile {
     public readonly id: string;
     public readonly service: service;
     public readonly externalId: string;
-    public readonly externalName: string | undefined;
-    public readonly externalEmail: string | undefined;
+    public readonly externalUsername: string | null;
+    public readonly externalEmail: string | null;
+    public readonly pfp: string | null;
 
-    constructor(service: service, id: string, externalId: string, externalName?: string, externalEmail?: string) {
+    constructor(service: service, id: string, externalId: string, externalName?: string, externalEmail?: string, pfp?: string) {
         this.service = service;
         this.id = id;
         this.externalId = externalId;
-        this.externalId = externalId;
-        this.externalName = externalName;
+        this.externalUsername = externalName;
         this.externalEmail = externalEmail;
+        this.pfp = pfp;
     }
 
     public static fromObject(object: any): Profile {
-        return new Profile(object.service, object.id, object.externalId, object.externalUsername, object.externalEmail)
+        return new Profile(object.service, object.id, object.externalId, object.externalUsername, object.externalEmail, object.pfp)
     }
 
 }
